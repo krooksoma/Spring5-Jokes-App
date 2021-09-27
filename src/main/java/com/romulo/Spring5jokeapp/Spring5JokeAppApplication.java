6e475de20c5ff8b0716844ac9359df5830a8010f
Romulo <romulojusto@gmail.com>
2021-09-27 13:48:16 -0400

    initial commit

---
+++ src/main/java/com/romulo/Spring5jokeapp/Spring5JokeAppApplication.java
@@ -0,0 +1,13 @@
+package com.romulo.Spring5jokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5JokeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romulo/Spring5jokeapp/Spring5JokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romulo.Spring5jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
